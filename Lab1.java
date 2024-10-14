87fc2248e072c080929403f6b64fac100dc92d7d
Андрей Мыценко <avm17022004@gmail.com>
2024-10-14 21:03:42 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -5,6 +5,7 @@ public class Lab1 {
 
     public static void main(String[] args) {
         System.out.println("Its works");
+        /** one more lane */
         System.out.println("Ну ладно добавлю еще строку");
     }
 }
