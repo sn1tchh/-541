041a26583d00f926b78e4b82615647639f4b6f61
Андрей Мыценко <avm17022004@gmail.com>
2024-10-18 08:36:40 +0300

    ЗАХИЩАЮСЬ

--- Lab1.java
+++ Lab1.java
@@ -7,5 +7,7 @@ public class Lab1 {
         System.out.println("Its works");
         /** one more lane */
         System.out.println("Ну ладно добавлю еще строку");
+        /** one more lane */
+        System.out.println("защищаюсь");
     }
 }
