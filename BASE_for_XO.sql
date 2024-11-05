59a8e4e518ccf4c1a74cb432dceaa3e034589d81
Андрей Мыценко <avm17022004@gmail.com>
2024-11-05 18:26:50 +0200

    Delete BASE_for_XO.sql

--- BASE_for_XO.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE myGAME;
-CREATE TABLE users(
-    id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(30) NOT NULL,
-    password VARCHAR(64) NOT NULL,
-    PRIMARY KEY(id)
-    );
-INSERT INTO users (username,password) VALUES ("Player1", "Pass1");
-INSERT INTO users (username,password) VALUES ("Player2", "Pass2");
