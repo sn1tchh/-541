51f25f3bd9c3fcea7d0340585713315b78bf73b9
Андрей Мыценко <avm17022004@gmail.com>
2024-11-05 22:33:41 +0200

    Create myGame.sql

---
+++ myGame.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE myGAME;
+USE myGAME;
+
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO users (username, password) VALUES ("Player1", "Pass1");
+INSERT INTO users (username, password) VALUES ("Player2", "Pass2");
